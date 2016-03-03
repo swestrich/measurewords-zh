e59f70f16d78e321bfda496c6d0b6f4e043e79df
Samuel Westrich <samuel.westrich@gmail.com>
2016-03-03 21:41:19 +0000

    fixed db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./DictionaryModel.sqlite',sqlite3.OPEN_READONLY);
+var db = new sqlite3.Database(__dirname + '/DictionaryModel.sqlite',sqlite3.OPEN_READONLY);
  
 module.exports = function(str,clb) {
   db.all("SELECT c.word from word a, classifier b, word c WHERE a.word = ? and a.id = b.word AND b.mw = c.id",[str], function(err, row) {
